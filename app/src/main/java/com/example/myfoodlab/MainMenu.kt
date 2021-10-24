4ddb920e2326503f2f072cda11fcb6bde58ba5c9
DINOSUN <89031340+DINOSUN@users.noreply.github.com>
2021-10-25 00:55:26 +0700

    edit commit

--- app/src/main/java/com/example/myfoodlab/MainMenu.kt
+++ app/src/main/java/com/example/myfoodlab/MainMenu.kt
@@ -3,9 +3,9 @@ package com.example.myfoodlab
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Confirmpassword : AppCompatActivity() {
+class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_confirmpassword)
+        setContentView(R.layout.activity_main_menu)
     }
 }
