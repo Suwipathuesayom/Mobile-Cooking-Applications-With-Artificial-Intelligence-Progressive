5446ef20f943a3e182bea025a7365cdba63e9c85
wittawat-jump <60682821+wittawat-jump@users.noreply.github.com>
2021-11-13 14:28:03 +0700

    rename comfilrmpassword

--- app/src/main/java/com/example/myfoodlab/ConfirmPassword.kt
+++ app/src/main/java/com/example/myfoodlab/ConfirmPassword.kt
@@ -3,7 +3,7 @@ package com.example.myfoodlab
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Confirmpassword : AppCompatActivity() {
+class ConfirmPassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirmpassword)
