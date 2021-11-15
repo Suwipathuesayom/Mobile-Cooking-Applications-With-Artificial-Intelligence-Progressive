952066cc43abeebf010bb80f026bb1e9b9e876b4
DINOSUN <89031340+DINOSUN@users.noreply.github.com>
2021-11-15 22:03:13 +0700

    frontendfourCommit

--- app/src/main/java/com/example/myfoodlab/MainMenu.kt
+++ app/src/main/java/com/example/myfoodlab/MainMenu.kt
@@ -4,6 +4,8 @@ import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import android.view.View
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
@@ -23,7 +25,39 @@ class MainMenu : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
 
+        val imageList = ArrayList<SlideModel>()
 
+        imageList.add(SlideModel("https://sv1.picz.in.th/images/2021/11/11/uLmT7l.png"))
+        imageList.add(SlideModel("https://sv1.picz.in.th/images/2021/11/11/uLmkqt.png"))
+        imageList.add(SlideModel("https://sv1.picz.in.th/images/2021/11/11/uLm38e.png"))
+
+        val imageSlider = findViewById<ImageSlider>(R.id.image_slider)
+        imageSlider.setImageList(imageList)
+
+        /*Handler(Looper.getMainLooper()).postDelayed({
+            aaa.visibility = View.GONE
+            bbb.visibility = View.GONE
+        }, 2*//*000)*/
+
+        //content.visibility = View.VISIBLE
+
+        bottomNavigationView2.setOnItemSelectedListener { position ->
+            when (position.itemId) {
+                R.id.menu -> {
+                    a1.visibility = View.VISIBLE
+                    a2.visibility = View.GONE
+                    //content.visibility = View.VISIBLE
+
+                }
+                R.id.filter -> {
+                    a1.visibility = View.GONE
+                    a2.visibility = View.VISIBLE
+                    //content.visibility = View.GONE
+                }
+            }
+            true
+
+        }
 
     }
 
